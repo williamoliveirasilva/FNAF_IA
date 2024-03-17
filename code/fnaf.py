50bbad1846b1652e52db30539ca14c2e895cd228
William Silva <121590646+williamoliveirasilva@users.noreply.github.com>
2024-03-17 13:04:39 -0300

    feature: criação de novas telas

--- code/fnaf.py
+++ code/fnaf.py
@@ -8,11 +8,39 @@ tela_largura = 1280
 tela_altura = 720
 
 tela = pygame.display.set_mode((tela_largura, tela_altura))
+pygame.display.set_caption('FNAF')
+relogio = pygame.time.Clock()
+cena = 1
+camera = '1a'
+
+pos_bonnie = '1a'
+pos_freddy = '1a'
+pos_chica = '1a'
+pos_foxy = '1c'
 
 while True:
+
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             exit()
-    pygame.display.update()
+        if event.type == KEYDOWN:
+            if event.key == K_SPACE:
+                tela.fill((0,0, 0))
+                if cena == 1:
+                    cena = 2
+                elif cena == 2:
+                    cena = 1
 
+    if cena == 1:
+        pygame.draw.rect(tela, (255, 0, 0), (400, 0, 100, 50))
+    elif cena == 2:
+        if camera == '1a':
+            if pos_chica == '1a':
+                pygame.draw.rect(tela, (255, 255, 0), (400, 400, 50, 50))
+            if pos_freddy == '1a':
+                pygame.draw.rect(tela, (139, 69, 19), (500, 400, 50, 50))
+            if pos_bonnie == '1a':
+                pygame.draw.rect(tela, (128, 0, 128), (300, 400, 50, 50))
+
+    pygame.display.update()
