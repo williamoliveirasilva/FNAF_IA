eb3e0693f3c3351e767b56d3a46ac97768ca74a2
William Silva <121590646+williamoliveirasilva@users.noreply.github.com>
2024-03-17 13:14:21 -0300

    feature: adição da tag de câmera (1a) e fonte personalizada

--- code/fnaf.py
+++ code/fnaf.py
@@ -9,6 +9,10 @@ tela_altura = 720
 
 tela = pygame.display.set_mode((tela_largura, tela_altura))
 pygame.display.set_caption('FNAF')
+
+caminho_fonte = "/fonts/pixChicago.ttf"
+fonte = pygame.font.Font(caminho_fonte, 22)
+
 relogio = pygame.time.Clock()
 cena = 1
 camera = '1a'
@@ -36,6 +40,12 @@ while True:
         pygame.draw.rect(tela, (255, 0, 0), (400, 0, 100, 50))
     elif cena == 2:
         if camera == '1a':
+            text_surface = fonte.render('Câmera 1a', True, (255,255,255))
+            rect = pygame.Rect(100, 100, 200, 100)
+            pygame.draw.rect(tela, (255,255,255), rect, 2)
+            text_rect = text_surface.get_rect(center=rect.center)
+            tela.blit(text_surface, text_rect)
+
             if pos_chica == '1a':
                 pygame.draw.rect(tela, (255, 255, 0), (400, 400, 50, 50))
             if pos_freddy == '1a':
