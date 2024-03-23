d878e6a28e0068c543dcfe75e3e5302a2cfce817
William Silva <121590646+williamoliveirasilva@users.noreply.github.com>
2024-03-22 22:14:36 -0300

    feature: adição de um sistema de níveis para os animatronics

--- fnaf.py
+++ fnaf.py
@@ -18,6 +18,7 @@ fundo = pygame.image.load("images/Background.png")
 tempo_inicial = pygame.time.get_ticks() // 1000
 contador_segundos = 0
 horario = 0
+num_noite = 1
 
 cena = 1
 camera = '1a'
@@ -27,8 +28,21 @@ pos_freddy = '1a'
 pos_chica = '1a'
 pos_foxy = '1c'
 
-pos_horaX = 50
-pos_horaY = 50
+bonnie_level = 0
+chica_level = 0
+freddy_level = 0
+fox_level = 0
+
+pos_horaX = 30
+pos_horaY = 80
+pos_noiteX = 30
+pos_noiteY = 20
+
+
+level_enhanced2 = False
+level_enhanced3 = False
+level_enhanced4 = False
+
 
 
 while True:
@@ -69,21 +83,45 @@ while True:
 
         tela.blit(fundo, (0, 0))
 
+        if num_noite == 1:
+            mostrar_noite = fonte.render('Night 1', True, (255, 255, 255))
+            tela.blit(mostrar_noite, (pos_noiteX, pos_noiteY))
+            bonnie_level = 0
+            chica_level = 0
+            fox_level = 0
+            freddy_level = 0
+
         if horario == 0:
             mostrar_horas = fonte.render('12 AM', True, (255, 255, 255))
             tela.blit(mostrar_horas, (pos_horaX, pos_horaY))
+            level_enhanced2 = False
+            level_enhanced3 = False
+            level_enhanced4 = False
         elif horario == 1:
             mostrar_horas = fonte.render('1 AM', True, (255, 255, 255))
             tela.blit(mostrar_horas, (pos_horaX, pos_horaY))
         elif horario == 2:
             mostrar_horas = fonte.render('2 AM', True, (255, 255, 255))
             tela.blit(mostrar_horas, (pos_horaX, pos_horaY))
+            if level_enhanced2 == False:
+                level_enhanced2 = True
+                bonnie_level += 1
         elif horario == 3:
             mostrar_horas = fonte.render('3 AM', True, (255, 255, 255))
             tela.blit(mostrar_horas, (pos_horaX, pos_horaY))
+            if level_enhanced3 == False:
+                level_enhanced3 = True
+                bonnie_level += 1
+                chica_level += 1
+                fox_level += 1
         elif horario == 4:
             mostrar_horas = fonte.render('4 AM', True, (255, 255, 255))
             tela.blit(mostrar_horas, (pos_horaX, pos_horaY))
+            if level_enhanced4 == False:
+                level_enhanced4 = True
+                bonnie_level += 1
+                chica_level += 1
+                fox_level += 1
         elif horario == 5:
             mostrar_horas = fonte.render('5 AM', True, (255, 255, 255))
             tela.blit(mostrar_horas, (pos_horaX, pos_horaY))
@@ -99,5 +137,6 @@ while True:
 
         if pos_foxy == '1c':
             pygame.draw.rect(tela, (255, 54, 54), (503, 309, 38, 38))
+
 
     pygame.display.update()
